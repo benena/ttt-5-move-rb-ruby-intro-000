2cc80e486c91cf5532738826f0b47e9b91663c6b
Benjamin Ko <benjamintko@gmail.com>
2020-03-03 07:13:24 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,12 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index(input)
+  input = input.to_i - 1
+end
+
+def move(array, what_is_defined_in_bin_doesnt_matter_in_this_argument, me = "X")
+  array[what_is_defined_in_bin_doesnt_matter_in_this_argument] = me
+end
+
