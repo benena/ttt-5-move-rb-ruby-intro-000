b4eb33161c0b71291008c780e8a31c40a9998f7a
Benjamin Ko <benjamintko@gmail.com>
2020-03-03 07:18:52 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -18,4 +18,4 @@ end
 
 # move(array, what_is_defined_in_bin_doesnt_matter_in_this_argument, me = "X")
   array[what_is_defined_in_bin_doesnt_matter_in_this_argument] = me
-# Originally used move(array, index, player_value = "X"), but had me confused due to the argument index being used as a variable in .bin
+# Originally used move(array, index, player_value = "X"), but had me confused due to the argument index being defined in .bin. the argument index seems to have no relationship with what's defined in .bin
